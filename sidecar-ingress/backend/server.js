f650b577194278469b9f737695926e2f72e893a9
samitkumarpatel <samitkumarpatel@gmail.com>
2021-04-20 14:47:22 +0200

    nginx sidecar act as fake ingress for FE

---
+++ sidecar-ingress/backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var os = require("os");
+
+var cors = require('cors')
+app.use(cors())
+
+app.get('/ping', (req, res) => {
+    res.json({
+        message: "pong"
+    })
+})
+
+app.get('/info', (req, res) => {
+    const p = require('./package.json')
+    res.json({
+        application: {
+            name: p.name,
+            version: p.version
+        },
+        hostname: os.hostname()
+    })
+})
+
+const PORT = process.env.SERVER_PORT | 3000;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
