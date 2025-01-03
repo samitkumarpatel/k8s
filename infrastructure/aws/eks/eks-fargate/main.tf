2e75f002c31c980c539e1c26820bf95bce4489fc
samitkumarpatel-work <SamitKumar.Patel@maersk.com>
2025-01-03 16:12:40 +0100

    fargate fix

--- infrastructure/aws/eks/eks-fargate/main.tf
+++ infrastructure/aws/eks/eks-fargate/main.tf
@@ -39,22 +39,6 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-# Private Subnets
-resource "aws_subnet" "eksfargate_private_subnet" {
-  count = 3
-
-  vpc_id                  = aws_vpc.eksfargate_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.eksfargate_vpc.cidr_block, 3, count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = false
-
-  tags = {
-    "Name"                                      = "${local.name}-private-${count.index + 1}"
-    "kubernetes.io/role/internal-elb"           = "1"
-    "kubernetes.io/cluster/${local.name}" = "owned"
-  }
-}
-
 # Public Subnets
 resource "aws_subnet" "eksfargate_public_subnet" {
   count = 3
@@ -66,10 +50,23 @@ resource "aws_subnet" "eksfargate_public_subnet" {
 
   tags = {
     Name                                        = "${local.name}-public-${count.index + 1}"
-    "kubernetes.io/role/elb"                    = "1"
-    "kubernetes.io/cluster/${local.name}" = "owned"
   }
 }
+
+# Private Subnets
+resource "aws_subnet" "eksfargate_private_subnet" {
+  count = 3
+
+  vpc_id                  = aws_vpc.eksfargate_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.eksfargate_vpc.cidr_block, 3, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    "Name"                                      = "${local.name}-private-${count.index + 1}"
+  }
+}
+
 
 # Internet Gateway
 resource "aws_internet_gateway" "eksfargate_igw" {
@@ -80,7 +77,7 @@ resource "aws_internet_gateway" "eksfargate_igw" {
   }
 }
 
-# Route Table for Public Subnets
+# Route Table for Public Subnets with Internet Gateway
 resource "aws_route_table" "eksfargate_public_rt" {
   vpc_id = aws_vpc.eksfargate_vpc.id
 
@@ -101,13 +98,17 @@ resource "aws_route_table_association" "eks_public_rt_assoc" {
   route_table_id = aws_route_table.eksfargate_public_rt.id
 }
 
+
+# NAT Gateway
 resource "aws_eip" "eksfargate_eip" {
   domain = "vpc"
-
+  depends_on = [ aws_internet_gateway.eksfargate_igw ]
+  
   tags = {
     Name = "${local.name}-nat-eip"
   }
 }
+
 
 resource "aws_nat_gateway" "eksfargate_nat" {
   allocation_id = aws_eip.eksfargate_eip.id
@@ -120,6 +121,7 @@ resource "aws_nat_gateway" "eksfargate_nat" {
   depends_on = [aws_internet_gateway.eksfargate_igw]
 }
 
+# Route Table for Private Subnets with NAT Gateway
 resource "aws_route_table" "eksfargate_private_rt" {
   vpc_id = aws_vpc.eksfargate_vpc.id
 
@@ -141,46 +143,42 @@ resource "aws_route_table_association" "eksfargate_private_rt_assoc" {
   route_table_id = aws_route_table.eksfargate_private_rt.id
 }
 
-# EKS IAM Role
-resource "aws_iam_role" "eksfargate_cluster_role" {
-  name = "${local.name}-cluster-im_role"
 
-  assume_role_policy = <<POLICY
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "eks.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  }
-  POLICY
+# EKS iAM Role
+data "aws_iam_policy_document" "eks-demo-cluster-admin-role-policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
 
-  tags = {
-    Name = "${local.name}-cluster-im-role"
+    actions = ["sts:AssumeRole"]
   }
 }
 
-resource "aws_iam_role_policy_attachment" "eksfargate_cluster_policy" {
-  role       = aws_iam_role.eksfargate_cluster_role.name
+resource "aws_iam_role" "eks-demo-cluster-admin-role-01" {
+  name               = "${local.name}-admin-role-01"
+  assume_role_policy = data.aws_iam_policy_document.eks-demo-cluster-admin-role-policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks-demo-cluster-01-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-demo-cluster-admin-role-01.name
 }
 
-# resource "aws_iam_role_policy_attachment" "eks_service_policy" {
-#   role       = aws_iam_role.eksfargate_cluster_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-# }
+resource "aws_iam_role_policy_attachment" "eks-demo-cluster-01-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks-demo-cluster-admin-role-01.name
+}
+
 
 
 # EKS Cluster
 resource "aws_eks_cluster" "eksfargate_cluster" {
   name      = "${local.name}-cluster"
-  #version  = 1.31 
-  role_arn  = aws_iam_role.eksfargate_cluster_role.arn
+  version  = 1.31 
+  role_arn  = aws_iam_role.eks-demo-cluster-admin-role-01.arn
 
   vpc_config {
     endpoint_private_access = false
@@ -193,13 +191,41 @@ resource "aws_eks_cluster" "eksfargate_cluster" {
     )
   }
 
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+
   tags = {
     Name = "${local.name}-cluster"
   }
 }
 
-# Fargate profile
+resource "aws_eks_addon" "eks-demo-addon-coredns" {
+  cluster_name                = aws_eks_cluster.eksfargate_cluster.name
+  addon_name                  = "coredns"
+  #addon_version               = "v1.10.1-eksbuild.4" 
+  resolve_conflicts_on_create = "OVERWRITE" 
+}
 
+resource "aws_eks_addon" "eks-demo-addon-kube-proxy" {
+  cluster_name                = aws_eks_cluster.eksfargate_cluster.name
+  addon_name                  = "kube-proxy"
+  #addon_version               = "v1.28.2-eksbuild.2" 
+  resolve_conflicts_on_create = "OVERWRITE" 
+}
+
+resource "aws_eks_addon" "eks-demo-addon-vpc-cni" {
+  cluster_name                = aws_eks_cluster.eksfargate_cluster.name
+  addon_name                  = "vpc-cni"
+  #addon_version               = "v1.15.1-eksbuild.1" 
+  resolve_conflicts_on_create = "OVERWRITE" 
+}
+
+
+
+# Fargate profile
 resource "aws_iam_role" "eksfargate_profile" {
   name = "${local.name}-profile"
 
@@ -222,16 +248,23 @@ resource "aws_iam_role_policy_attachment" "eksfargate_iam_policy_profile" {
 
 resource "aws_eks_fargate_profile" "kube-system" {
   cluster_name           = aws_eks_cluster.eksfargate_cluster.name
-  fargate_profile_name   = "kube-system"
+  fargate_profile_name   = "${local.name}-kube-system"
+  
   pod_execution_role_arn = aws_iam_role.eksfargate_profile.arn
-
-  # These subnets must have the following resource tag: 
-  # kubernetes.io/cluster/<CLUSTER_NAME>.
+  
   subnet_ids = aws_subnet.eksfargate_private_subnet[*].id
 
   selector {
     namespace = "kube-system"
+    labels = {
+      "k8s-app" = "kube-dns"
+    }
   }
+  
+  selector {
+    namespace = "default"
+  }
+
 }
 
 
